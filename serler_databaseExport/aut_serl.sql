9ecd2b3fd18ac3b76b231bc500c54d3a98fbe4e0
yangjackascd <yangjackascd@gmail.com>
2015-05-28 01:42:13 +1200

    upload function upgrade - can be read after upload it, add 2 more field to enter detail upgrade search function -can search by name, class , year and author upgrade search result - add more detail : rating , journal , click name can open in new page with full report upgrade database structure

--- serler_databaseExport/aut_serl.sql
+++ serler_databaseExport/aut_serl.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2015-05-27 05:07:22
+-- Generation Time: 2015-05-27 15:38:43
 -- 服务器版本： 5.6.21
 -- PHP Version: 5.5.19
 
@@ -35,20 +35,24 @@ CREATE TABLE IF NOT EXISTS `paper_table` (
   `class_experience_level` varchar(20) DEFAULT NULL,
   `paper_upload_date` varchar(20) DEFAULT NULL,
   `username` varchar(20) DEFAULT NULL,
-  `status` varchar(2) DEFAULT NULL COMMENT '0 mean reject,1 mean accept  2 mean hold'
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `status` varchar(2) DEFAULT NULL COMMENT '0 mean reject,1 mean accept  2 mean hold',
+  `rating` int(11) NOT NULL,
+  `paper_journal` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- 转存表中的数据 `paper_table`
 --
 
-INSERT INTO `paper_table` (`paper_name`, `paper_id`, `author`, `paper_title`, `year`, `class_experience_level`, `paper_upload_date`, `username`, `status`) VALUES
-('The Need for Speed: Automating Acceptance Testing in an eXtreme Programming Environment', 1, ' Lisa Crispin, Tip House, Carol Wade', 'ATDD', '2001', 'Professional', '2009-02-23 19:49:00.', 'rwampler', '1'),
-('hospitality', 2, 'Jake', 'HY', '2014', 'Professional', '2015-05-01 00:00:00.', 'jackascd', '1'),
-('software development ', 3, 'Jake', 'SD', '2014', 'Professional', '2015-05-13 00:00:00.', 'jackascd', '1'),
-('software interestment ', 4, 'Jake', 'SI', '2014', 'Professional', '2015-05-02 00:00:00.', 'jackascd', '1'),
-('Here', 5, 'jake', 'Software', '2014', NULL, '05/27/2015 01:32:38 ', 'jackascd', '2'),
-('womabi', 6, 'ninini', 'nimabi', '12345', NULL, '05/27/2015 02:52:25 ', 'jackascd', '2');
+INSERT INTO `paper_table` (`paper_name`, `paper_id`, `author`, `paper_title`, `year`, `class_experience_level`, `paper_upload_date`, `username`, `status`, `rating`, `paper_journal`) VALUES
+('The Need for Speed: Automating Acceptance Testing in an eXtreme Programming Environment', 1, ' Lisa Crispin, Tip House, Carol Wade', 'ATDD', '2001', 'Professional', '2009-02-23 19:49:00.', 'rwampler', '1', 5, 'USA'),
+('hospitality', 2, 'Jake', 'HY', '2014', 'Professional', '2015-05-01 00:00:00.', 'jackascd', '1', 1, ''),
+('software development ', 3, 'Jake', 'Soft', '2014', 'Professional', '2015-05-13 00:00:00.', 'jackascd', '1', 2, 'England'),
+('software interestment ', 4, 'Jake', 'Ecom', '2014', 'Professional', '2015-05-02 00:00:00.', 'jackascd', '1', 3, 'China'),
+('Here', 5, 'jake', 'Software', '2014', NULL, '05/27/2015 01:32:38 ', 'jackascd', '2', 4, 'Aerospace America'),
+('womabi', 6, 'ninini', 'nimabi', '12345', NULL, '05/27/2015 02:52:25 ', 'jackascd', '2', 3, 'Aerospace America'),
+('Software Team work', 7, 'Jake', 'TKT', '2013', NULL, '05/27/2015 11:32:15 ', 'jackascd', '2', 4, 'Aerospace America'),
+('Learn English', 8, 'Alax', 'ABC', '2015', NULL, '05/28/2015 12:19:18 ', 'jackascd', '2', 2, 'AUT make');
 
 -- --------------------------------------------------------
 
@@ -95,7 +99,7 @@ ALTER TABLE `user_table`
 -- AUTO_INCREMENT for table `paper_table`
 --
 ALTER TABLE `paper_table`
-MODIFY `paper_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `paper_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
